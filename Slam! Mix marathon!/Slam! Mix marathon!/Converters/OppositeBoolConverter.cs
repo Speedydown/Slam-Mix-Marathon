5c8d118b621b0577b0a1d66ba85e283c0e32e1fb
Teake Otter <teake@wiezitwaarvandaag.nl>
2016-01-01 22:08:00 +0100

    werkende versie

---
+++ Slam! Mix marathon!/Slam! Mix marathon!/Converters/OppositeBoolConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Slam__Mix_Marathon.Converters
+{
+    /// <summary>
+    /// Converts a Boolean into a Visibility.
+    /// </summary>
+    public class OppositeBoolConverter : IValueConverter
+    {
+        /// <summary>
+        /// If set to True, conversion is reversed: True will become Collapsed.
+        /// </summary>
+        public bool IsReversed { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var val = System.Convert.ToBoolean(value);
+            if (this.IsReversed)
+            {
+                val = !val;
+            }
+
+            return !val;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
